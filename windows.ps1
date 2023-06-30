cc2829c2550ad17ea44630febac96cb6fd61a411
Raisson Souto <raisson.souto@ccc.ufcg.edu.br>
2023-06-30 12:31:59 -0300

    Reorganizing project and windows.ps1 is now done.

--- windows.ps1
+++ windows.ps1
@@ -34,29 +34,34 @@ $tasks = foreach ($program in $programs) {
 }
 
 Wait-Job $tasks | Out-Null
-
-$results = $tasks | Receive-Job
-
-foreach ($i in 0..($results.Count - 1)) {
-    $program = $programs[$i]
-    $result = $results[$i]
-
-    if ($result) {
-        Write-Host "Uninstalled program: $program"
-    } else {
-        Write-Host "Program not found: $program"
-    }
-}
-
+Receive-Job $tasks | Out-Null
 Remove-Job $tasks | Out-Null
+
+# Set small buttons in taskbar
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarSmallIcons" -Value 1
+
+# Hide the search button in taskbar
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "SearchboxTaskbarMode" -Value 0
+
+# Hide the Cortana button
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowCortanaButton" -Value 0
+
+# Hide the Task View button
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Value 0
+
+# Hide News and Interests
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Feeds" -Name "ShellFeedsTaskbarVisibility" -Value 0
+
+# Restart the explorer process to apply the changes
+Stop-Process -Name explorer
 
 # Set Windows dark theme
 if (!(Test-Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize')) {
-  New-Item -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize' -Force
+  New-Item -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize' -Force | Out-Null
 }
 
-New-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize' -Name 'AppsUseLightTheme' -Value 0 -Type DWord -Force
-New-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize' -Name 'SystemUsesLightTheme' -Value 0 -Type DWord -Force
+New-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize' -Name 'AppsUseLightTheme' -Value 0 -Type DWord -Force | Out-Null
+New-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize' -Name 'SystemUsesLightTheme' -Value 0 -Type DWord -Force | Out-Null
 
 # Set desktop wallpaper
 Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name 'Wallpaper' -Value 'C:\Windows\System32\solidblack.bmp'
@@ -67,13 +72,12 @@ $nvidiaDriver = "D:\Softwares\installers\driver-nvdia.exe"
 
 if (Test-Path $nvidiaDriver -PathType Leaf) {
     Start-Process -FilePath $nvidiaDriver
+    Write-Host "Installing Nvidia driver"
 } else {
     "File does not exist: $nvidiaDriver"
 }
 
 workflow Install-WSL {
-    param ([string]$Name)
-
     # Check if WSL is already installed
     if (!(Get-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online -ErrorAction SilentlyContinue)) {
     
@@ -92,25 +96,25 @@ workflow Install-WSL {
 
     # Download Ubuntu 22.04 LTS from the Microsoft Store
     $app = Get-AppxPackage -Name CanonicalGroupLimited.Ubuntu22.04onWindows
-    if ($app -eq $null) {
-        Start-Process -FilePath "ms-windows-store://pdp/?productid=9P91RK0MGV6M" -Verb RunAs
-        Write-Host "Please install Ubuntu 22.04 LTS from the Microsoft Store and run this script again."
-        Pause
-        Exit
-    }
 
-    # Launch Ubuntu 22.04 LTS and complete the installation
-    wsl --set-version Ubuntu-22.04 2
-    wsl -d Ubuntu-22.04
+    if ($app -eq $null) {
+        Write-Host "Error installing Ubuntu 22, make it manually."
+    } else {
+        # Launch Ubuntu 22.04 LTS and complete the installation
+        wsl --set-version Ubuntu-22.04 2
+        wsl -d Ubuntu-22.04
+    }
 }
 
 Install-WSL
 
 # Install openvpn
-Invoke-WebRequest -Uri "https://openvpn.net/downloads/openvpn-client-installer.exe" -OutFile "C:\Temp\openvpn-client-installer.exe"
-Start-Process "C:\Temp\openvpn-client-installer.exe"
+Write-Host "Installing OpenVPN"
+Invoke-WebRequest -Uri "https://swupdate.openvpn.org/community/releases/OpenVPN-2.6.5-I001-amd64.msi" -OutFile "C:\Temp\openvpn-client-installer.msi"
+Start-Process "C:\Temp\openvpn-client-installer.msi"
 
 # Install Chocolatey
+Write-Host "Installing Chocolatey"
 if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
     Set-ExecutionPolicy Bypass -Scope Process -Force;
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
@@ -138,6 +142,7 @@ $programs = @(
 
 # Install programs in parallel
 $tasks = foreach ($program in $programs) {
+    Write-Host "Installing $args[0]..."
     Start-Job -ScriptBlock { choco install $args[0] -y } -ArgumentList $program
 }
 
--- wsl.sh
+++ wsl.sh
@@ -1,23 +1,11 @@
-sudo apt-get update -y &> ~/update.log
-sudo apt-get upgrade -y &> ~/update.log
+sudo apt-get update -y &> /dev/null
+sudo apt-get upgrade -y &> /dev/null
 
 # Install programming languages and general tools
-sudo apt-get install -yy git python3 python3-pip golang-go &> /dev/null
 sudo apt-get install -yy ca-certificates curl gnupg lsb-release &> /dev/null
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list &> /dev/null
-
-sudo apt-get update -y &> /dev/null
-sudo apt-get install -yy docker-ce docker-ce-cli containerd.io &> /dev/null
+sudo apt-get install -yy git python3 python3-pip &> /dev/null
 
 # Install security tools
-sudo apt-get install -yy netcat wireshark libimage-exiftool-perl &> /dev/null
-
-# Add current user to the docker group and restart the shell
-sudo groupadd docker &> /dev/null
-sudo usermod -aG docker "${USER}" &> /dev/null
-newgrp docker &> /dev/null
-sudo chmod 666 /var/run/docker.sock &> /dev/null
+sudo apt-get install -yy netcat libimage-exiftool-perl &> /dev/null
 
 echo "WSL 2 setup complete!"
