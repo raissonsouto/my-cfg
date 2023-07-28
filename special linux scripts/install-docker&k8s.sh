ab79a6e025e35de98930158aacf4d0f0354d1155
Raisson Souto <raisson.souto@ccc.ufcg.edu.br>
2023-07-28 10:30:23 -0300

    Updating k8s installation script

--- special linux scripts/install-docker&k8s.sh
+++ special linux scripts/install-docker&k8s.sh
@@ -2,38 +2,57 @@
 # Raisson Souto, 2023
 
 # The goal of this script is to automate the installation
-# of docker and kubernetes at Ubuntu
+# of kubernetes (1.27) at Ubuntu (22.04)
 
-apt-get update -y &> /dev/null
-apt-get install ca-certificates curl gnupg lsb-release apt-transport-https -yy &> /dev/null
+# Log file
+LOG_FILE="logs.log"
 
-distribution=(cat /etc/os-release | grep -oP '(?<=^ID=).+' | tr -d '"')
-version=(cat /etc/os-release | grep -oP '(?<=^VERSION_CODENAME=).+' | tr -d '"')
+# Disable swap
+sudo sed -ri '/\sswap\s/s/^#?/#/' /etc/fstab
+sudo swapoff -a
 
-curl -fsSL https://download.docker.com/linux/$distribution/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$distribution $version stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-apt-get update -y &> /dev/null
-apt-get install -yy docker-ce docker-ce-cli containerd.io &> /dev/null
-
-cat << EOF | sudo tee /etc/modules-load.d/k8s.conf
+# Kernel modules installation
+cat << EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
 br_netfilter
 EOF
 
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Configuration of sysctl parameters
+cat << EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
 EOF
 
-sysctl --system
+# Apply configurations on sysctl
+sudo sysctl --system &> /dev/null 2>> $LOG_FILE
 
+# Installing prerequisites 
+sudo apt-get update -y &> /dev/null 2>> $LOG_FILE
+sudo apt-get install ca-certificates curl gnupg lsb-release apt-transport-https -yy &> /dev/null 2>> $LOG_FILE
+
+# Getting Docker GPG key
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Installing containerd
+sudo apt update
+sudo apt install containerd.io -yy
+
+# Configuring containerd
+sudo mkdir -p /etc/containerd && containerd config default | sudo tee /etc/containerd/config.toml
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+sudo systemctl restart containerd
+
+# Getting K8S GPG key
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-apt-get update -y &> /dev/null
-apt-get install -yy kubeadm kubelet kubectl &> /dev/null #  kubeadm=1.23.4-00 kubelet=1.23.4-00 kubectl=1.23.4-00
-apt-mark hold kubelet kubeadm kubectl &> /dev/null
-
-systemctl enable docker
-systemctl daemon-reload
-systemctl restart docker
+# Installing K8S
+apt-get update -y &> /dev/null 2>> $LOG_FILE
+apt-get install -yy kubeadm=1.27.0-00 kubelet=1.27.0-00 kubectl=1.27.0-00 &> /dev/null 2>> $LOG_FILE
+apt-mark hold kubelet kubeadm kubectl &> /dev/null 2>> $LOG_FILE
