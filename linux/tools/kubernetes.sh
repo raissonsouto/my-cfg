133048b80760557d9c61563e025579bb8a8c33c2
Raisson Souto <raisson.souto@ccc.ufcg.edu.br>
2025-01-30 16:25:11 -0300

    Update kubernetes.sh

--- linux/tools/kubernetes.sh
+++ linux/tools/kubernetes.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-sudo sed -ri '/\sswap\s/s/^#?/#/' /etc/fstab
+export DEBIAN_FRONTEND="noninteractive"
+#sed -i "s/#\$nrconf{kernelhints} = -1;/\$nrconf{kernelhints} = -1;/g" /etc/needrestart/needrestart.conf // used to disable pending kernel upgrade popup
+
+sudo apt update -qq
+sudo apt upgrade -yy -qq
+
+sudo apt-get install -yy -qq apt-transport-https ca-certificates curl gpg gnupg lsb-release
+
 sudo swapoff -a
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
 
 cat << EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
@@ -19,22 +27,21 @@ EOF
 
 sudo sysctl --system
 
-sudo apt-get update -yy
-sudo apt-get install ca-certificates curl gnupg lsb-release apt-transport-https -yy
-
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | \
+    sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-sudo apt update
-sudo apt install containerd.io -yy
+sudo apt update -qq
+sudo apt install containerd.io -yy -qq
 
 sudo mkdir -p /etc/containerd && containerd config default | sudo tee /etc/containerd/config.toml
-sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
 sudo systemctl restart containerd
 
-apt-get install -yy kubeadm=1.27.0-00 kubelet=1.27.0-00 kubectl=1.27.0-00
-apt-mark hold kubelet kubeadm kubectl
+sudo apt-get install -yy -qq kubeadm kubelet kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
