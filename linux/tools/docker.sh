239cada8ba73b9f9ee252e93cc348d45fa4c8f52
Raisson Souto <raissonsouto@gmail.com>
2025-01-31 08:31:37 -0300

    Update on tools scripts

--- linux/tools/docker.sh
+++ linux/tools/docker.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
 
-# Update package lists
-sudo apt update
+sudo apt update -qq
+sudo apt install -qq -yy apt-transport-https ca-certificates curl gnupg lsb-release
 
-# Install dependencies
-sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+    sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-# Add Docker's official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
+    https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Set up the stable Docker repository
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -qq
+sudo apt install -qq -yy docker-ce docker-ce-cli containerd.io
 
-# Update the package database with the Docker packages from the newly added repo
-sudo apt update
-
-# Install Docker
-sudo apt install -y docker-ce docker-ce-cli containerd.io
-
-# Add current user to the docker group to execute Docker commands without sudo (optional)
 sudo usermod -aG docker $USER
+sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s \
+    https://api.github.com/repos/docker/compose/releases/latest | grep -oP \
+    '"tag_name": "\K(.*)(?=")')/docker-compose-$(uname -s)-$(uname -m)" -o \
+    /usr/local/bin/docker-compose
 
-# Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-# Apply executable permissions to the Docker Compose binary
 sudo chmod +x /usr/local/bin/docker-compose
-
-# Display Docker and Docker Compose versions
-echo "Docker installed:"
-docker --version
--- linux/tools/golang.sh
+++ linux/tools/golang.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-echo_topic "Installing Golang..."
-
 VERSION=$(curl -sL https://golang.org/VERSION?m=text | head -n 1 | awk '{print $1}')
-wget https://go.dev/dl/$VERSION.src.tar.gz
-tar -C /usr/local -xzf $VERSION.linux-amd64.tar.gz
+wget https://go.dev/dl/$VERSION.linux-amd64.tar.gz -O ~/$VERSION.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf ~/$VERSION.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.profile
 source $HOME/.profile
-rm $VERSION.linux-amd64.tar.gz
-
-echo_topic "Golang installed"
+rm ~/$VERSION.linux-amd64.tar.gz
